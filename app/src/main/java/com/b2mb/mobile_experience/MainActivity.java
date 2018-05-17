f3a6bef4fabbca02062f59f66d41db1796f15f1f
Whisper <gewoonjvr@gmail.com>
2018-05-17 18:50:08 +0200

    [Added] onClickListener to ?.

--- app/src/main/java/com/b2mb/mobile_experience/MainActivity.java
+++ app/src/main/java/com/b2mb/mobile_experience/MainActivity.java
@@ -1,15 +1,27 @@
 package com.b2mb.mobile_experience;
 
+import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button help;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        help = findViewById(R.id.startHelpButton);
+        help.setOnClickListener((View) -> {
+            Intent intent = new Intent(getApplicationContext(), HelpActivity.class);
+            startActivity(intent);
+        });
+
     }
 }
